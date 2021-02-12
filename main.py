8e0160b8b1863f4cb66d5a92e301339200c2d7d6
Jason Guest <jason@rgrmarketing.com>
2021-02-12 15:37:35 -0800

    More commit

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import requests
 from keys import TOKEN
 pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "xsist14"
 user_params = {
     "token": TOKEN,
-    "username": "xsist14",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "graph1",
+    "name": "Quran Graph",
+    "unit": "minutes",
+    "type": "int",
+    "color": "ajisai"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
