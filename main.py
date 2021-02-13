f07c420c73d2eb2a1a6ac4121b5f53d2176cb343
xsist14 <jason.d.guest@gmail.com>
2021-02-12 17:05:46 -0800

    finished the initial project

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 from keys import TOKEN
+
+from datetime import *
+
 pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = "xsist14"
 user_params = {
@@ -22,4 +25,28 @@ graph_config = {
 headers = {
     "X-USER-TOKEN": TOKEN
 }
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+today = date.today()
+today = today.strftime('%Y%m%d')
+
+pixel_config = {
+    "quantity": "10",
+    "date": today
+}
+
+
+# response = requests.post(pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
+update_config = {
+    "quantity": "0",
+}
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1/{today}"
+# response = requests.put(update_endpoint, json=update_config, headers=headers)
+# response.raise_for_status()
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1/{today}"
+response = requests.delete(delete_endpoint, headers=headers)
+response.raise_for_status()
+print(response.text)
